fb2b1c54e0c08e1d9ef6b5ea0359f20c036717e2
x1210x <x1210x@gmai.com>
2022-10-07 14:37:24 +0900

    타이머

--- lib/ui/home/tab2/tab2.dart
+++ lib/ui/home/tab2/tab2.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_template/bloc/timer_bloc.dart';
+import 'package:flutter_template/ticker.dart';
+import 'package:flutter_template/ui/home/tab2/timer_view.dart';
 
 class Tab2Page extends StatefulWidget {
   const Tab2Page({Key? key}) : super(key: key);
@@ -10,13 +14,9 @@ class Tab2Page extends StatefulWidget {
 class Tab2PageState extends State<Tab2Page> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[
-          Text("Tab2")
-        ],
-      )
+    return BlocProvider(
+      create: (_) => TimerBloc(ticker: const Ticker()),
+      child: const TimerView()
     );
   }
 }
